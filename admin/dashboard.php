a469c8f46d5f0903fe96789b4c8cfa8265e19519
Mulkan <protogamerss@gmail.com>
2022-04-26 13:20:09 +0800

    pemanggilan pada component

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -9,15 +9,16 @@
 <!doctype html>
 <html lang="en">
 
-
+    <?php include "componen/head.php" ?>
 
 <body>
 
+<?php include "componen/header.php" ?>
 
   <div class="container-fluid">
     <div class="row">
       
-    
+    <?php include "componen/nav.php" ?>
 
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
