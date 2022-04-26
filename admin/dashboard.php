5ce1c9e39c76bf1da9b3c575c914d4c7e5fa9442
Mulkan <protogamerss@gmail.com>
2022-04-26 14:16:31 +0800

    pembagian coding dan penambahan file main.php dan routes.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,24 +1,3 @@
-<?php
-  session_start();
-  include_once"../database/koneksi.php";
-  if (isset($_SESSION['username'])) {
-    
-?>
-
-
-<!doctype html>
-<html lang="en">
-
-    <?php include "componen/head.php" ?>
-
-<body>
-
-<?php include "componen/header.php" ?>
-
-  <div class="container-fluid">
-    <div class="row">
-      
-    <?php include "componen/nav.php" ?>
 
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -156,21 +135,3 @@
           </table>
         </div>
       </main>
-    </div>
-  </div>
-
-
-  <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-
-  <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
-  <script src="../assets/dashboard.js"></script>
-</body>
-
-</html>
-
-<?php
-    }else {
-      header('location: ../index.php');
-    }
-?>
---
+++ admin/main.php
@@ -0,0 +1,39 @@
+<?php
+  session_start();
+  include_once"../database/koneksi.php";
+  if (isset($_SESSION['username'])) {
+    
+?>
+
+<!doctype html>
+<html lang="en">
+
+    <?php include "componen/head.php" ?>
+
+<body>
+
+    <?php include "componen/header.php" ?>
+
+  <div class="container-fluid">
+    <div class="row">  
+        <?php include "componen/nav.php" ?>
+
+        <?php include "routes.php" ?>
+    </div>
+  </div>
+
+
+  <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
+  <script src="../assets/dashboard.js"></script>
+</body>
+
+</html>
+
+<?php
+    }else {
+      header('location: ../index.php');
+    }
+?>
