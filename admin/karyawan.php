557fd91be84252bd901d4d51fe6bdd52105f879c
Mulkan <protogamerss@gmail.com>
2022-05-12 21:26:09 +0800

    penambahan btn Data Baru

--- admin/karyawan.php
+++ admin/karyawan.php
@@ -6,6 +6,7 @@
       </div>
   </div>
   <div class="table-responsive">
+    <a href="?page=karyawantambah" class="btn btn-success mb-3"><span data-feather="plus"></span>Data Baru</a>
         <table class="table table-striped table-sm">
           <thead>
             <tr>
